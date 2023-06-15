0c527e5f9eee9afb7914e3970c71a0466aeb75d1
Wagner Andrade <wsawebmaster@yahoo.com.br>
2023-06-15 18:52:07 -0300

    WA - Modificando dados em cada página e ativando menu

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,18 +1,41 @@
 import { Request, Response } from 'express';
 
+import { createMenuObject } from '../helpers/createMenuObject';
+
 export const home = (req: Request, res: Response) => {
-  res.send('home no controller');
-  // res.render('pages/page');
+  // res.send('home no controller');
+  res.render('pages/page', {
+    menu: createMenuObject('all'),
+    banner: {
+      title: 'Todos os animais',
+      background: 'allanimals.jpg'
+    }
+  });
 }
 export const dogs = (req: Request, res: Response) => {
-
-  // res.render('pages/page');
-}
+  // res.send('dogs');
+  res.render("pages/page", {
+    menu: createMenuObject('dog'),
+    banner: {
+      title: "Cachorros",
+      background: "banner_dog.jpg",
+    },
+  });}
 export const cats = (req: Request, res: Response) => {
-
-  // res.render('pages/page');
-}
+  // res.send("cats");
+  res.render("pages/page", {
+    menu: createMenuObject('cat'),
+    banner: {
+      title: "Gatos",
+      background: "banner_cat.jpg",
+    },
+  });}
 export const fishes = (req: Request, res: Response) => {
-
-  // res.render('pages/page');
-}
+  // res.send("fishes");
+  res.render("pages/page", {
+    menu: createMenuObject('fish'),
+    banner: {
+      title: "Peixes",
+      background: "banner_fish.jpg",
+    },
+  });}
